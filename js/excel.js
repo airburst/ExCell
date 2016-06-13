5f9b93dea8f51d945b39605c0ab96ed70427f42c
airburst <mark@fairhursts.net>
2016-06-13 17:03:55 +0100

    Added Comment class and comment data extraction methods.

--- js/excel.js
+++ js/excel.js
@@ -44,13 +44,90 @@ class Excell {
             for (let sheet in worksheet) {
                 /* all keys that do not begin with "!" correspond to cell addresses */
                 if (sheet[0] === '!') continue;
+                if (this.hasComment(worksheet[sheet])) { this.getCommentData(worksheet[sheet].c); }
                 output += (name + '!' + sheet + '=' + JSON.stringify(worksheet[sheet])) + '\n';
             }
         };
         return output;
     }
+
+    hasComment(cell) {
+        return cell.c !== undefined;
+    }
+
+    getCommentData(commentText) {
+        let comment = new Comment(commentText[0].t.toString());
+        comment.getData();
+        console.log(comment);
+    }
 }
 
+
+
+class Comment {
+    constructor(commentText = '') {
+        this.fullText = commentText;
+        this.name = '';
+        this.type = '';
+        this.dataType = 'single';
+        this.rows = 0;
+        this.cols = 0;
+    }
+
+    lines() {
+        return this.fullText.split('\n');
+    }
+
+    pairs() {
+        let pairs = [],
+            self = this;
+        for (let line of this.lines()) {
+            let pair = line.split(':');
+            if (hasTwoParts(pair)) { pairs.push(pair); }
+        }
+        return pairs;
+
+        function hasTwoParts(pair) {
+            return (pair.length > 1) && !self.isBlank(pair[1]);
+        }
+    }
+
+    isValid() {
+        return !this.isBlank(this.type);
+    }
+
+    isBlank(str) {
+        return (!str || /^\s*$/.test(str));
+    }
+
+    getData() {
+        for (let p of this.pairs()) {
+            this.setType(p);
+            this.setDataType(p);
+            this.setRows(p);
+            this.setCols(p);
+        }
+    }
+
+    setType(p) {
+        if ((p[0] === 'I') || (p[0] === 'O')) {
+            this.type = p[0];
+            this.name = p[1];
+        }
+    }
+
+    setDataType(p) {
+        if (p[0] === 'dataType') { this.dataType = p[1]; }
+    }
+
+    setRows(p) {
+        if (p[0] === 'rows') { this.rows = p[1]; }
+    }
+
+    setCols(p) {
+        if (p[0] === 'cols') { this.cols = p[1]; }
+    }
+}
 
 let xl = new Excell();
 let details = xl.processFile('checker.xlsx');
