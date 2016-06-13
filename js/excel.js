ac728011fb11519125f1f6e7b952a214a41c21ab
airburst <mark@fairhursts.net>
2016-06-13 17:23:24 +0100

    Add comments into Excell class

--- js/excel.js
+++ js/excel.js
@@ -1,37 +1,10 @@
 const XLSX = require('xlsx');
-// Cell Object
-
-// Key	Description
-// v	raw value (see Data Types section for more info)
-// w	formatted text (if applicable)
-// t	cell type: b Boolean, n Number, e error, s String, d Date
-// f	cell formula (if applicable)
-// r	rich text encoding (if applicable)
-// h	HTML rendering of the rich text (if applicable)
-// c	comments associated with the cell **
-// z	number format string associated with the cell (if requested)
-// l	cell hyperlink object (.Target holds link, .tooltip is tooltip)
-// s	the style/theme of the cell (if applicable)
-
-// Data Types
-// The raw value is stored in the v field, interpreted based on the t field.
-// Type b is the Boolean type. v is interpreted according to JS truth tables
-// Type e is the Error type. v holds the number and w holds the common name:
-
-// Value	Error Meaning
-// 0x00	#NULL!
-// 0x07	#DIV/0!
-// 0x0F	#VALUE!
-// 0x17	#REF!
-// 0x1D	#NAME?
-// 0x24	#NUM!
-// 0x2A	#N/A
-// 0x2B	#GETTING_DATA
 
 class Excell {
 
     constructor() {
         this.data = [];
+        this.comments = [];
     }
 
     processFile(fileName) {
@@ -58,10 +31,13 @@ class Excell {
     getCommentData(commentText) {
         let comment = new Comment(commentText[0].t.toString());
         comment.getData();
-        console.log(comment);
+        this.addComment(comment);
+    }
+
+    addComment(comment) {
+        if (comment.isValid()) { this.comments.push(comment); }
     }
 }
-
 
 
 class Comment {
@@ -129,9 +105,12 @@ class Comment {
     }
 }
 
-let xl = new Excell();
-let details = xl.processFile('checker.xlsx');
-document.getElementById('excel-file').innerText = details;
+// Main
+(function() {
+    let xl = new Excell();
+    let details = xl.processFile('checker.xlsx');
+    document.getElementById('excel-file').innerText = details;
+})();
 
 /* set up drag-and-drop event */
 // function handleDrop(e) {
