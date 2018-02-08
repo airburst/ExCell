0331bf0a9b6d81057bbf7dbb24e12895511f9d1b
Mark Fairhurst <mark@Marks-MBP.lan>
2018-02-08 11:20:45 +0000

    fix(eslint): Add exception for no-plusplus

--- src/services/parser.js
+++ src/services/parser.js
@@ -1,15 +1,14 @@
 import formula from 'excel-formula';
 
 class Parser {
-
   constructor() {
     this.getTokens = formula.getTokens;
   }
 
-  getRangeTokens(formula = '') {
-    return this.getTokens(formula)
-      .filter((t) => { return t.subtype === 'range' })
-      .map((r) => { return r.value; });
+  getRangeTokens(expression = '') {
+    return this.getTokens(expression)
+      .filter(t => t.subtype === 'range')
+      .map(r => r.value);
   }
 }
 
