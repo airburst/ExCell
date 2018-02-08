c50c384bdd60b8e85cf8e268195f5940e4b44ebe
airburst <mark@fairhursts.net>
2018-02-08 09:06:42 +0000

    feat(tests): Added some rudimentary tests for formula

---
+++ src/services/formula.test.js
@@ -0,0 +1,37 @@
+/* eslint-env jest */
+import { run } from 'formula';
+import { dp } from './utils';
+
+const inputs = {
+  age: 48,
+  weight: 83.4,
+  height: 1.85,
+};
+const sumArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+/***
+ * NOTE: The formula library contains a full set of
+ * unit tests. This test set is a minimal amount
+ * to prove confidence in the way that the app
+ * will make use of formula as a solver.
+ */
+describe('Formula module', () => {
+  test('correctly calculates using specified inputs', () => {
+    expect(run('=(220-age)', inputs)).toBe(172);
+    expect(dp(run('=(weight/(height*height))', inputs))).toBe('24.36');
+  });
+
+  test('cannot calculate using inferred inputs', () => {
+    const a = 50;
+    expect(run('=(220-a)')).toMatchObject(new Error(
+      { name: 'Invalid value', message: '#VALUE!' }
+    ));
+  });
+
+  test('correctly calculates using array inputs', () => {
+    expect(run('=SUM(sumArray)', { sumArray })).toBe(21);
+  });
+});
--- src/services/utils.js
+++ src/services/utils.js
@@ -9,3 +9,30 @@ export const arrayToString = array => {
         ? JSON.stringify(item) + '\n'
         : item + '\n');
 }
+
+export const castNumber = num => {
+    const cast = parseFloat(num, 10);
+    if (isNaN(cast)) { return 0; }
+    return cast;
+}
+
+export const dp = (num, places = 2) => {
+    const n = castNumber(num);
+    const nn = Math.floor(n * Math.pow(10, places)) / Math.pow(10, places);
+    return nn.toFixed(places);
+}
+
+export const roundUpToPenny = (amount) => {
+    if (!amount) { return 0; }
+    return Math.ceil((castNumber(amount) - 0.0000001) * 100) / 100;
+}
+
+export const roundDownToPenny = (amount) => {
+    if (!amount) { return 0; }
+    return Math.floor(castNumber(amount) * 100) / 100;
+}
+
+export const roundUpToPound = (amount) => {
+    if (!amount) { return 0; }
+    return parseInt(Math.ceil(castNumber(amount)), 10);
+}
