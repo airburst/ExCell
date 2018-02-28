a11e9b610a04feda0fc5d1c6e35363c716a39fb4
airburst <mark@fairhursts.net>
2018-02-28 10:23:19 +0000

    styles(Main)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import './Header.css';
 
-const Header = props => (
+const Header = () => (
   <div className="header">
-    <Link to="/load" className="left">
+    <Link to="/load" className="left" title="Click to load new Sheet">
       <Icon name="file excel outline" size="large" />
       <div className="title">ExCell</div>
     </Link>
   </div>
 );
-
-// Header.propTypes = {
-//   title: PropTypes.string.isRequired,
-// };
 
 export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import { Header, Load, Code, TestModel, Main } from './components';
+import { Header, Main, Load } from './components';
 import './index.css';
 
 // Add new routes here
@@ -8,8 +8,6 @@ const Routes = () => (
   <Switch>
     <Route path="/" exact component={Main} />
     <Route path="/load" exact component={Load} />
-    <Route path="/model" exact component={TestModel} />
-    <Route path="/code" exact component={Code} />
   </Switch>
 );
 
